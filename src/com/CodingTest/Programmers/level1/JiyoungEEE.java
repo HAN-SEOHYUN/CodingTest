f28b4aa9ea3c280d7d4bcff8875995a850205add
HAN-SEOHYUN <000312@daum.net>
2022-11-25 00:13:12 +0900

    SelectIcecream

---
+++ src/com/CodingTest/Programmers/level1/JiyoungEEE.java
@@ -0,0 +1,14 @@
+package com.CodingTest.Programmers.level1;
+
+
+public class JiyoungEEE {
+
+    public static int age = 10;
+    public static  String name ="최지영";
+    public static  String result = name + "은 " + age + " 살 입니다";
+
+    public static void main(String[] args) {
+        System.out.println(result);
+    }
+
+}
